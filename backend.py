2a6552d9a91ebf7c21fb198171b52adbc340885a
mendel <mendelwaks@maimonidesschool.org>
2024-03-26 12:56:53 -0400

    backend checked by amh

--- backend.py
+++ backend.py
@@ -7,6 +7,10 @@ root.title("Drink Sale")
 #gc = gspread.service_account(filename=r"C:\Users\waksm\Downloads\drinksaleapp.json")
 gc = gspread.service_account(filename=r"C:\Users\mhdsc\Downloads\drinksaleapp.json")
 
+Keep_pick = tkinter.PhotoImage(file="assets/buttons for ds.png").subsample(16, 16)
+cancel_pick = tkinter.PhotoImage(file="assets/buttons for ds (1).png").subsample(16, 16)
+Reload = tkinter.PhotoImage(file="assets/buttons for ds (2).png").subsample(16,16)
+
 sh = gc.open_by_url(
         "https://docs.google.com/spreadsheets/d/1uRiz65hyUq0bcF1FSpKUb0coWActURXMXKHfB9hiLDg/"
     )
@@ -34,7 +38,7 @@ def refresh():
     # lets say we have ["a", "b", "c"]
     # calling enumerate 
     # [.. (2, "c")]
-    orders = [f for f in enumerate(ws.col_values(1))][2::]
+    orders = [f for f in enumerate(ws.col_values(1))][1::]
     # tuple is an index into the spreadsheet and the corresponding data for the order
     # return button to delete the order
     def make_order(tuple):
@@ -81,17 +85,33 @@ def refresh():
 
             ws.delete_rows(tuple[0]+1) 
             refresh()
+        def cancel():
+            ws.delete_rows(tuple[0]+1) 
+            refresh()
         order_button = tkinter.Button(text=tuple[1],  command=order_done)
-        shut_up_button = tkinter.Button(text=tuple[1],  command=dealete)
-        return order_button
+        shut_up_button = fortnite_sucks = tkinter.Button(image=cancel_pick,  command=cancel)
+        
+        return (order_button, shut_up_button)
     # lets say we have [1, 2 , 3]
     # lets say we have a function that takes in a number and adds 1 to it (add1)
     # [1,2,3].map(add1) = [2, 3, 4]
 
     orders = map(make_order, orders)
+    button_row = 0
     for order in orders:
-        buttons.append(order)
-        order.pack()
+        # pack the confrim button
+        buttons.append(order[0]) # we use brackets to get things from a tuple
+        order[0].grid(row=button_row, column=0)
+        # pack the cancel button for this order
+        buttons.append(order[1]) # we use brackets to get things from a tuple
+        order[1].grid(row=button_row, column=1)
+        button_row = button_row +1
+    brawl_stars_rocks = REALOAD = tkinter.Button(image=Reload,  command=refresh)
+
+    REALOAD.grid(row=button_row)
+    buttons.append(REALOAD)
+
+    
 
 
 ws.delete_rows(tuple[0]+1) 
